ef48a16072960d94fc51e6f2a62c112d1bacaa8a
Micmelev <137012485+Micmelev@users.noreply.github.com>
2023-09-01 09:02:37 -0400

    Update script.js
    Added Bank

--- script.js
+++ script.js
@@ -15,19 +15,19 @@ document.getElementById('subjectName').onchange = function(){
   let name1 = name.value
   if (name1 === "Investment Authorization") {
     form = ["Recent notes related to outgoing money", "Recent incoming money in correct account",
-      "Notes in form tab", "Fees match", "Signed/Dated Form", "Do we have the asset", "For PP does investment amount match what's being invested", "Are there wiring wiring instructions in the asset and does is match", "Beneficiary name matches", "If sending check does address match", "Check if Multi Member", "Is amount under 800K (if not needs a verbal)"]
+      "Notes in form tab", "Fees match", "Signed/Dated Form", "Do we have the asset", "For PP does investment amount match what's being invested", "Are there wiring wiring instructions in the asset and does is match", "Beneficiary name matches", "Bank Name Matches","If sending check does address match", "Check if Multi Member", "Is amount under 800K (if not needs a verbal)"]
   }
   else if (name1 === "Distribution") {
-    form = ["Recent notes related to outgoing money", "Recent incoming money in correct account", "Notes in form tab", "Fees match", "Signed/Dated Form", "Beneficiary name matches", "If sending check does address match", "Is amount under 800K", "Is there a task for account closure"]
+    form = ["Recent notes related to outgoing money", "Recent incoming money in correct account", "Notes in form tab", "Fees match", "Signed/Dated Form", "Beneficiary name matches","Bank Name Matches", "If sending check does address match", "Is amount under 800K", "Is there a task for account closure"]
   }
   else if (name1 === "EPR") {
-    form = ["Recent notes related to outgoing money", "Recent incoming money in correct account", "Notes in form tab", "Fees match", "Signed/Dated Form", "Do we have the asset", "Beneficiary name matches", "If sending check does address match", "Is amount under 800K(if not needs a verbal)", "Does the invoice have the correct titling and asset"]
+    form = ["Recent notes related to outgoing money", "Recent incoming money in correct account", "Notes in form tab", "Fees match", "Signed/Dated Form", "Do we have the asset", "Beneficiary name matches","Bank Name Matches", "If sending check does address match", "Is amount under 800K(if not needs a verbal)", "Does the invoice have the correct titling and asset"]
   }
   else if (name1 === "Broad EPR") {
     form = ["Recent notes related to outgoing money", "Recent incoming money in correct account", "Notes in form tab", "Fees match", "Signed/Dated Form"]
   }
   else if (name1 === "Outgoing Transfer") {
-    form = ["Recent notes related to outgoing money", "Recent incoming money in correct account", "Notes in form tab", "Fees match", "Signed/Dated Form", "Beneficiary name matches", "If sending check does address match", "Is amount under 800K", "Is there a task for account closure"]
+    form = ["Recent notes related to outgoing money", "Recent incoming money in correct account", "Notes in form tab", "Fees match", "Signed/Dated Form", "Beneficiary name matches", "Bank Name Matches","If sending check does address match", "Is amount under 800K", "Is there a task for account closure"]
   }
   else form = []
   generateList()
